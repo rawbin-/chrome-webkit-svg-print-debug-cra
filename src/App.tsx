a58d091b0f19d3c18d523334f8b873b0e2355f96
rawbin.zhang <rawbin.zhang@lunarax.io>
2022-02-17 15:58:20 +0800

    feat: 添加LineDemo对比

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PreviewPrintTest from "./debug-svg-print/PreviewPrintTest";
+import DemoLine from "./DemoLine";
 
 function App() {
   return (
     <div className="App">
+        <DemoLine></DemoLine>
       <PreviewPrintTest></PreviewPrintTest>
     </div>
   );
